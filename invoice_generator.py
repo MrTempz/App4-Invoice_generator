b231d4a04a65e62e6aabc488685ddf85e80364de
Adam <adamtemplin92@gmail.com>
2022-12-08 22:27:51 +0100

    Day26 - Add table with invoice content

--- invoice_generator.py
+++ invoice_generator.py
@@ -3,6 +3,12 @@ import os
 from pathlib import Path
 from fpdf import FPDF
 
+INVOICE_COLUMNS={0: {'name': 'product_id', 'size': 30, 'ln': 0},
+                 1: {'name': 'product_name', 'size': 70, 'ln': 0}, 
+                 2: {'name': 'amount_purchased', 'size': 30, 'ln': 0},
+                 3: {'name': 'price_per_unit', 'size': 30, 'ln': 0},
+                 4: {'name': 'total_price', 'size': 30, 'ln': 1}
+}
 
 
 def add_header(pdf: FPDF, invoice_file_path: str):
@@ -15,9 +21,26 @@ def add_header(pdf: FPDF, invoice_file_path: str):
     pdf.cell(w=50, h=8, txt=f'Invoice date {invoice_date}', ln=1)
 
 
+def add_table(pdf: FPDF, invoice_df: pd.DataFrame):
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+
+    for column in INVOICE_COLUMNS.values():
+        pdf.cell(w=column['size'], h=8, 
+            txt=column['name'].replace('_', ' ').title(), 
+            border=1, ln=column['ln'])
+
+    pdf.set_font(family='Times', size=10)
+    for index, row in invoice_df.iterrows():
+        for column in INVOICE_COLUMNS.values():
+            pdf.cell(w=column['size'], h=8, txt=str(row[column['name']]),
+                border=1, ln=column['ln'])
+
+
 def generate_invoice(invoice_df: pd.DataFrame, invoive_file_path: str) -> FPDF:
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     add_header(pdf, invoive_file_path)
+    add_table(pdf, invoice_df=invoice_df)
 
     return pdf
 
